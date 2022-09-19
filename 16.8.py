409034b4d6e65e4a95c5d3892202d0e1c2c80274
Kyle Price <90288361+Kpricescript@users.noreply.github.com>
2022-09-18 21:28:18 -0500

    Add files via upload

---
+++ 16.8.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn = sqlite3.connect('books.db')
+curs = conn.cursor()
+curs.execute('''CREATE TABLE books
+    (title TEXT,
+     author TEXT,
+     year INTEGER
+     )''')
+conn.commit()     
